adc47d4079103905d3ab5a409607e45c366c9241
lbsr <laura.soria.simon@gmail.com>
2015-05-05 18:54:56 +0200

    Despliegue DB

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,9 +2,9 @@ var models = require('../models/models.js');
 
 //GET /quizes/question
 exports.question = function(req,res) {
-	models.Quiz.findAll().success(function(quiz){
+   models.Quiz.findAll().success(function(quiz){
 	res.render('quizes/question', {pregunta: quiz[0].pregunta});
-	})
+   })
 };
 
 //GET /quizes/answer
--- models/models.js
+++ models/models.js
@@ -31,14 +31,14 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; //exportar definicion de tabla quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().then(function(){
+sequelize.sync().success(function(){
 // success(..) ejecuta el manejador una vez creada la tabla
-        Quiz.count().then(function (count){
+        Quiz.count().success(function (count){
 		if(count === 0) {  //la tabla se inicializa solo si esta vacia
 		Quiz.create({ pregunta: 'Capital de Italia',
 					  respuesta: 'Roma'
 		})
-		.then(function(){console.log('Base de datos inicializada')});
+		.success(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
